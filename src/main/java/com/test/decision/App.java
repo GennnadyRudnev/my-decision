9e824f8334bf7738ffe76f0eabb71586a6eab883
Gennady <gangrav@gmail.com>
2018-09-09 21:37:29 +0300

    добавил readme и предложение ввести команду

--- src/main/java/com/test/decision/App.java
+++ src/main/java/com/test/decision/App.java
@@ -9,44 +9,40 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-            System.out.println("Введите команду и параметры(если имеются), например: search(команда) absolutePathToFile nameFile");
-            System.out.println("Для вывода списка команд введите: list");
-            System.out.println("");
+        String[] comands = args;
 
-            String[] comands = args;
+        switch (comands[1]) {
 
-            switch (comands[1]) {
+            case "list":
+                System.out.println();
+                for (Commands command : Commands.values()) {
+                    System.out.println(command);
+                }
+                break;
 
-                case "list":
-                    System.out.println();
-                    for (Commands command : Commands.values()) {
-                        System.out.println(command);
-                    }
-                    break;
+            case "readDrives":
+                Drives.readDrives();
+                break;
 
-                case "readDrives":
-                    Drives.readDrives();
-                    break;
+            case "search":
+                if (comands.length > 2) {
+                    FilesUtil.getNewFiliUtil().searchFiles(comands[2], comands[3], false);
+                }
+                break;
 
-                case "search":
-                    if (comands.length > 2) {
-                        FilesUtil.getNewFiliUtil().searchFiles(comands[2], comands[3], false);
-                    }
-                    break;
+            case "modified":
+                if (comands.length > 2) {
+                    FilesUtil.getNewFiliUtil().modifiedFile(comands[2], comands[3]);
+                }
+                break;
 
-                case "modified":
-                    if (comands.length > 2) {
-                        FilesUtil.getNewFiliUtil().modifiedFile(comands[2], comands[3]);
-                    }
-                    break;
-
-                default:
-                    System.out.println();
-                    for (Commands command : Commands.values()) {
-                        System.out.println(command);
-                    }
-                    break;
-            }
+            default:
+                System.out.println();
+                for (Commands command : Commands.values()) {
+                    System.out.println(command);
+                }
+                break;
         }
     }
+}
 
